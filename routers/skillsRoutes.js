b55022a78c251e4da5914f33ff0d0a9bbbc41bb8
AINaviHaja <haja@gpsglobal.co.uk>
2026-04-08 02:32:47 +0100

    Update routers/skillsRoutes.js

--- routers/skillsRoutes.js
+++ routers/skillsRoutes.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   getSkillsProfile,
   addSkill,
   updateSkill,
   deleteSkill,
-  getRelevanceScore,
+  calculateRelevanceScore,
   getGapAnalysis,
-} = require('../controllers/skillsController');
+} = require("../controllers/skillsController");
 
-router.get('/profile', getSkillsProfile);
-router.post('/', addSkill);
-router.put('/:id', updateSkill);
-router.delete('/:id', deleteSkill);
-router.post('/relevance-score', getRelevanceScore);
-router.post('/gap-analysis', getGapAnalysis);
+router.get("/skills-profile", getSkillsProfile);
+router.post("/", addSkill);
+router.put("/:id", updateSkill);
+router.delete("/:id", deleteSkill);
+router.post("/relevance-score", calculateRelevanceScore);
+router.post("/gap-analysis", getGapAnalysis);
 
 module.exports = router;
